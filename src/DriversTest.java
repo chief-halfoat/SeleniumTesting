fdaefc862388d191b6f13545a824a6b38dc812fe
chief-halfoat <johnathan.j.hart@gmail.com>
2022-03-15 20:12:15 -0400

    testing

---
+++ src/DriversTest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriversTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
